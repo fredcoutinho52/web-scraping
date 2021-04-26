fd06a923a7c30bc98689ad2850c02fda03fa3e91
Fred Coutinho <42944398+fredcoutinho52@users.noreply.github.com>
2021-04-26 10:22:16 -0300

    writing data on csv file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
+from unidecode import unidecode
 
+# link of python jobs from programathor website
 url = 'https://programathor.com.br/jobs-python'
 
 # getting url content and parsing with beautifulsoup
@@ -20,5 +23,15 @@ jobs_location = [job.find_all('span')[1].text for job in jobs_description]
 # mounting dataframe to better visualization
 jobs_df = {'job': jobs, 'place': jobs_place, 'location': jobs_location}
 jobs_df = pd.DataFrame(data=jobs_df)
-
 print(jobs_df)
+
+# writing data on csv file
+with open('jobs.csv', 'w', newline='') as file:
+  writer = csv.writer(file)
+  writer.writerow(['Job', 'Place', 'Location'])
+  for index, value in enumerate(jobs):
+    writer.writerow([
+      unidecode(value),
+      unidecode(jobs_place[index]),
+      unidecode(jobs_location[index])
+    ])
